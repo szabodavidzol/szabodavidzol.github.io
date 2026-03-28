210b6fe2601297c705ac573f5c6e8f7a64e93466
Dávid Szabó <szabo.david.zol@gmail.com>
2026-03-28 17:08:32 +0100

    Projekt frissités 1

--- szabo-david-projekt/js/javascript.js
+++ szabo-david-projekt/js/javascript.js
@@ -10,3 +10,22 @@ cards.forEach(card => {
         card.classList.add('active');
     });
 });
+
+
+
+
+function nextImage(slideshow) {
+    const images = slideshow.querySelectorAll('img');
+
+    let currentIndex = 0;
+
+    images.forEach((image, index) => {
+        if (!image.classList.contains('hide')) {
+            currentIndex = index;
+        }
+        image.classList.add('hide');
+    });
+
+    let nextIndex = (currentIndex + 1) % images.length;
+    images[nextIndex].classList.remove('hide');
+}
